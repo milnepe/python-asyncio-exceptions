3e3116e2e3aea673576959e467615c9140131f3f
Peter Milne <milnepe@gmail.com>
2025-06-30 09:15:18 +0100

    Add timers

--- serial.py
+++ serial.py
@@ -1,18 +1,23 @@
 import asyncio
+import time
 
 from utils import FooError, foo
 
 async def main():
+    start_t = time.monotonic()
     try:
         result1 = await foo(1)          # wait for the result of foo(1)
+        print(f"coro 1 finish: {time.monotonic() - start_t}")
     except FooError as e:
         result1 = e
 
     try:
         result2 = await foo(2)          # wait for the result of foo(2)
+        print(f"coro 2 finish: {time.monotonic() - start_t}")
     except FooError as e:
         result2 = e
-    
+
+    print(f"coros finished: {time.monotonic() - start_t}")    
     print(f"foo 1 result: {result1}")
     print(f"foo 2 result: {result2}")
 
