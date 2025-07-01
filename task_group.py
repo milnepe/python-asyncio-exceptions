6649a17769fd10e47b420992e843c255231151ea
Peter Milne <milnepe@gmail.com>
2025-07-01 08:49:54 +0100

    Always print elapsed time

--- task_group.py
+++ task_group.py
@@ -1,26 +1,34 @@
+"""
+Example of 2 tasks executing concurrently
+If one fails the other tasks are cancelled
+NOTE: Requires python v3.11+
+"""
+
 import asyncio
 import time
 
 from utils import FooError, foo
 
+
 async def main():
-    
     try:
         start_t = time.monotonic()
         async with asyncio.TaskGroup() as tg:
-            task_1 = tg.create_task(foo(1))    # task 1 starts running
-            task_2 = tg.create_task(foo(2))    # task 2 starts running
-        # context waits for all tasks to finish - no need to await them!
+            task_1 = tg.create_task(foo(1))  # task 1 starts running
+            task_2 = tg.create_task(foo(2))  # task 2 starts running
+            # context waits for all tasks to finish - no need to await them!
     except* FooError as eg:
         for e in eg.exceptions:
             print(e)
     else:
-        print(f"task 1 finish: {time.monotonic() - start_t}")
+        # Only print if successful
         print(f"foo 1 result: {task_1.result()}")
-        print(f"task 2 finish: {time.monotonic() - start_t}")
+        print(f"task 1 finish: {time.monotonic() - start_t}")
         print(f"foo 2 result: {task_2.result()}")
+        print(f"task 2 finish: {time.monotonic() - start_t}")
 
-        print(f"tasks finished: {time.monotonic() - start_t}")
+    print(f"tasks finished: {time.monotonic() - start_t}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
